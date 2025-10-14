307612cd2d24a5d0eedbdc964737e8794a69d6ab
TarinaSnyman <tarina.snyman@gmail.com>
2025-10-14 12:43:00 +0200

    Update Student Repository
    Updated it to include password.
    Created an interface and extended to it

--- Repositories/StudentRepository.js
+++ Repositories/StudentRepository.js
@@ -1,7 +1,9 @@
+const IStudentRepository=require('./IStudentRepository')
 const MongoDBConnection = require('../Model/database/connection');
 
-class StudentRepository {
+class StudentRepository extends IStudentRepository {
   constructor() {
+    super();
     this.dbConnection = new MongoDBConnection();
     this.collection = null;
   }
@@ -10,18 +12,18 @@ class StudentRepository {
     if (!this.collection) {
       await this.dbConnection.connect();
       const db = this.dbConnection.getDatabase();
-      this.collection = db.collection('students');
+      this.collection = db.collection('Student');
     }
   }
 
-  // find student by email
-  async findByEmail(email) {
+  // find student by email and password
+  async findByEmailAndPassword(email, password) {
     try {
       await this.init();
-      const student = await this.collection.findOne({ Email: email });
+      const student = await this.collection.findOne({ Email: email, Password: password });
       return student;
     } catch (error) {
-      console.error('Error finding student by email:', error);
+      console.error('Error finding student by email and password:', error);
       throw error;
     }
   }
