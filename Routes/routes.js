9d247372ff4d24203dacabd0eefe7b485e50a992
Phasma12-wq <dawbe.mapre@gmail.com>
2025-10-16 11:30:58 +0200

    added chatbot routes

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,5 +1,8 @@
 const express = require('express'); 
+
 const { loginUser } = require('../Controller/loginController');
+
+const { renderChat, chatAPI } = require('../Controller/chatController');
 
 const router = express.Router();
 
@@ -10,5 +13,11 @@ router.get('/login', (req, res) => res.render('login'));
 router.post('/login', loginUser);
 
 router.get('/forgot', (req, res) => res.render('forgotpassword'));
+
+router.get('/api/chat/chatroom', renderChat );
+
+router.post('/api/chat', chatAPI );
+
+
 
 module.exports = router;
