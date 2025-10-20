5e286b260051e9caab03dd241c817cecc17918b0
DSReaper <123795058+DSReaper@users.noreply.github.com>
2025-10-20 08:32:32 +0200

    Add findByEmail method to IStudentRepository

--- Repositories/IStudentRepository.js
+++ Repositories/IStudentRepository.js
@@ -2,7 +2,9 @@ class IStudentRepository{
     async findByEmailAndPassword(email,password){
         throw new Error("Method not implemented");
     }
-
+    async findByEmail(email){
+        throw new Error("Method not implemented")
+    }
 }
 
 module.exports=IStudentRepository;
